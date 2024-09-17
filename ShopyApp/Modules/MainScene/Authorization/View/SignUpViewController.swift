4113f83650137e98d5df738397fa404eb50470ec
Elsayed Mansour <sayedmansoo123@gmail.com>
2024-09-17 15:54:14 +0300

    update sign up and its view model and logic

--- ShopyApp/Modules/MainScene/Authorization/View/SignUpViewController.swift
+++ ShopyApp/Modules/MainScene/Authorization/View/SignUpViewController.swift
@@ -15,18 +15,163 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     @IBOutlet weak var passwordConfirmation: UITextField!
     
-    
+    var _firstName, _lastName, _email, _password: String!
+    let userDefualt = Utilities()
+    var signUpViewModel : SignUpViewModel?
+    var customers : [CustomerModel]? = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        configureLoadingData()
+        bindToViewModel()
     }
     
-
+    func bindToViewModel(){
+        signUpViewModel?.bindNavigate = { [weak self] in
+            DispatchQueue.main.async {
+                if ((self?.signUpViewModel?.navigateToHome()) == true){
+                    self?.navigate()
+                }
+                else{
+                    let alert = UIAlertController(title: "Not Authorized", message: "An Error Occured", preferredStyle: .alert)
+                    let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                    alert.addAction(gotIt)
+                    self?.present(alert, animated: true)
+                }
+            }
+        }
+    }
+    
+    func navigate(){
+        if self.signUpViewModel?.navigate == true{
+            performSegue(withIdentifier: "toHome", sender: self)
+        }else {
+            print("failed")
+            
+        }
+    }
+    
+    func configureLoadingData(){
+        print("configureLoadingData")
+        signUpViewModel = SignUpViewModel()
+        print("1")
+        signUpViewModel?.loadData()
+        print("2")
+        signUpViewModel?.bindResultToViewController = { [weak self] in
+            DispatchQueue.main.async {
+                self?.customers = self?.signUpViewModel?.getAllCustomers() ?? []
+                print("customers")
+                print(self?.customers?[0].email ?? "")
+                guard let customers = self?.signUpViewModel?.getAllCustomers() else {
+                    return
+                }
+                self?.customers = customers
+                
+                
+            }
+        }
+    }
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true)
     }
+    @IBAction func signUpButton(_ sender: Any) {
+        
+        signUpViewModel?.checkNetworkReachability{ isReachable in
+            if isReachable {
+                self._firstName = self.firstName.text
+                self._lastName = self.secondName.text
+                self._email = self.emailAddress.text
+                self._password = self.password.text
+                if self._firstName != "" && self._lastName != ""{
+                    if self.userDefualt.isValidEmail(self._email){
+                        if self.passwordConfirmation.text == self._password{
+                            if self._password.count >= 6{
+                                
+                                
+                                for item in (self.signUpViewModel?.getAllCustomers())! {
+                                    let comingMail = item.email ?? ""
+                                    if comingMail == self._email{
+                                        print("matched EMail:\(comingMail)")
+                                        self.signUpViewModel?.flag=true
+                                    }
+                                }
+                                if self.signUpViewModel?.flag == true{
+                                    let alert = UIAlertController(title: "Warning", message: "This user already exists", preferredStyle: .alert)
+                                    let gotIt = UIAlertAction(title: "Ok", style: .cancel)
+                                    let signUp = UIAlertAction(title: "Sign In", style: .default) { UIAlertAction in
+                                        self.performSegue(withIdentifier: "toSign", sender: nil)
+                                    }
+                                    
+                                    alert.addAction(signUp)
+                                    alert.addAction(gotIt)
+                                    self.present(alert, animated: true)
+                                    
+                                }else{
+                                    
+                                    
+                                    self.signUpViewModel?.registerCustomer(firstName: self._firstName , lastName: self._lastName , email: self._email, password: self._password){ result in
+                                        
+                                        print("signUp View controller.registerCustomer")
+                                        switch result{
+                                        case true:
+                                            print("from view  \(String(describing: self._firstName ?? ""))")
+                                        case false:
+                                            DispatchQueue.main.async {
+                                                let alert = UIAlertController(title: "Warning", message:"Error in registering" , preferredStyle: .alert)
+                                                let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                                                alert.addAction(gotIt)
+                                                self.present(alert, animated: true)
+                                            }
+                                        }
+                                    }
+                                }
+                            }else{
+                                let alert = UIAlertController(title: "Warning", message:"Password must be more than 5 digit" , preferredStyle: .alert)
+                                let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                                alert.addAction(gotIt)
+                                self.present(alert, animated: true)
+                            }
+                        }else{
+                            
+                            let alert = UIAlertController(title: "Warning", message: "Password Confirmation Doesnt match", preferredStyle: .alert)
+                            let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                            alert.addAction(gotIt)
+                            self.present(alert, animated: true)
+                            
+                        }
+                        
+                    }else{
+                        let alert = UIAlertController(title: "Warning", message: "Please, enter valid email", preferredStyle: .alert)
+                        let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                        alert.addAction(gotIt)
+                        self.present(alert, animated: true)
+                        
+                    }
+                }else{
+                    let alert = UIAlertController(title: "Warning", message: "Required full name", preferredStyle: .alert)
+                    let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                    alert.addAction(gotIt)
+                    self.present(alert, animated: true)
+                    
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.showAlert()
+                }
+            }
+        }
+        
+    }
     
-
+    func showAlert(){
+        let alertController = UIAlertController(title: "No Internet Connection", message: "Check your network and try again", preferredStyle: .alert)
+        
+        let doneAction = UIAlertAction(title: "Retry", style: .cancel)
+        
+        alertController.addAction(doneAction)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
 }
--- ShopyApp/Modules/MainScene/Authorization/ViewModel/SignUpViewModel.swift
+++ ShopyApp/Modules/MainScene/Authorization/ViewModel/SignUpViewModel.swift
@@ -6,3 +6,237 @@
 //
 
 import Foundation
+
+class SignUpViewModel{
+    let userDefualt = Utilities()
+    
+    let model = ReachabilityManager()
+    
+    var networkHandler:NetworkManager?
+    
+    var bindResultToViewController : (()->()) = {}
+    
+    var customers : [CustomerModel] = []
+    
+    var flag: Bool = false
+
+    var bindNavigate:(()->()) = {}
+    
+    var bindDontNavigate:(()->()) = {}
+    
+    var wishlistDraftId : Int?
+    
+    var cartDraftId : Int?
+    
+    var note : String = ""
+    
+    var navigate: Bool! {
+        didSet{
+            bindNavigate()
+        }
+    }
+    
+    func navigateToHome()-> Bool{
+        return navigate
+    }
+    
+    var result : CustomerModel?{
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    
+    init() {
+        self.networkHandler = NetworkManager()
+    }
+    
+    var listOfCustomer : [CustomerModel]?{
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    func getAllCustomers()-> [CustomerModel]?{
+        return listOfCustomer
+    }
+    
+    func loadData() {
+        let apiUrl = APIHandler.urlForGetting(.customers)  //"https://a73c5fc1c095fd186d957dd2093e9006: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@q2-24-team2.myshopify.com/admin/api/2024-01/customers.json?since_id=1"
+        print(apiUrl)
+        networkHandler?.fetchData(url: apiUrl, type: AllCustomers.self, complitionHandler: { data in
+            //print("the data from fetching all customers\(data?.customers?.count ?? 0)")
+            if let data = data{
+                self.listOfCustomer = data.customers
+                print("no of customers in load data\(self.listOfCustomer?.count)")
+            }else {
+                print("error in getting all customers")
+                
+            }
+        }, headers: [
+            "Cookie":"",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        ])
+    }
+    
+    func checkNetworkReachability(completion: @escaping (Bool) -> Void) {
+        model.checkNetworkReachability { isReachable in
+            completion(isReachable)
+        }
+    }
+    
+    func registerCustomer(firstName: String, lastName: String, email: String, password: String, completion: @escaping (Bool) -> Void) {
+        self.createDraftOrder()
+        Thread.sleep(forTimeInterval: 1)
+       // self.note = "\(self.cartDraftId ?? 0),\(self.wishlistDraftId ?? 0)"
+        if flag == false {
+            
+            let customer = CustomerModel(
+                first_name: firstName,
+                last_name: lastName,
+                email: email,
+                phone: nil,
+                tags: password,
+                id: nil,
+                verified_email: true,
+                note: ""
+            )
+            
+            self.registerNewCustomer(customer: customer) { result in
+                switch result {
+                case true:
+                    
+                    var customerNote: [String: [String: String]] = [
+                        "customer": [
+                            "note":"\(self.cartDraftId ?? 0),\(self.wishlistDraftId ?? 0)"
+                        ]
+                    ]
+                    let apiUrl = APIHandler.urlForGetting(.Customer(id: String(self.userDefualt.getCustomerId())))
+                    
+                    self.networkHandler?.putInApi(url: apiUrl, parameters:customerNote)
+             
+                    self.navigate = true
+                    completion(true)
+                    
+                case false:
+                    self.navigate = false
+                    completion(false)
+                    
+                    
+                }
+            }
+        } else {
+            self.navigate = false
+            completion(false)
+        }
+    }
+    
+    func registerNewCustomer(customer: CustomerModel, completion: @escaping (Bool)->Void ){
+        guard let newCustomer = HelperFunctions().convertToDictionary(object: customer, string: "customer") else { return }
+        print(newCustomer)
+        let apiUrl = APIHandler.urlForGetting(.customers)
+        
+        
+        networkHandler?.postWithResponse(url: apiUrl, type: Customer.self, parameters: newCustomer) {
+            customerContainer in
+            
+            print("calling PostCustomerToAPi")
+            
+            if let savedCustomer = customerContainer?.customer {
+                let id = savedCustomer.id ?? 0
+                print("1:\(id)\n")
+                
+                let customerName = savedCustomer.first_name ?? ""
+                print("1:\(customerName)\n")
+                
+                let customerLastName = savedCustomer.last_name ?? ""
+                print("1:\(customerLastName)\n")
+                
+                let customerEmail = savedCustomer.email ?? ""
+                print("1:\(customerEmail)\n")
+                
+                let customerPassword = savedCustomer.tags ?? ""
+                print("1:\(customerPassword)\n")
+                
+                let customerNote = savedCustomer.note ?? "0"
+                print("1:\(customerPassword)\n")
+                
+                if id != 0 {
+                    
+                    self.userDefualt.login()
+                    self.userDefualt.addCustomerId(id: id)
+                    
+                    self.userDefualt.addCustomerEmail(customerEmail: customerEmail)
+                    self.userDefualt.addCustomerName(customerName: "\(customerName) \(customerLastName)")
+                    self.userDefualt.login()
+                    self.userDefualt.setUserPassword(password: customerPassword)
+                    
+                    self.userDefualt.setUserNote(note: "\(self.cartDraftId ?? 0),\(self.wishlistDraftId ?? 0)")
+                    print(self.userDefualt.getUserNote())
+                    self.userDefualt.setCartID(cartId: String(self.cartDraftId ?? 0))
+                    print(self.userDefualt.getCartID())
+                    self.userDefualt.setWishlistID(wishlistId: String(self.wishlistDraftId ?? 0))
+                    print(self.userDefualt.getWishlistID())
+                    print("Password User:\(String(describing: self.userDefualt.getUserPassword()))")
+                    print("url:\(apiUrl)")
+                    print("put in api : cartId:\(self.cartDraftId ?? 0) wishlist Id :\(self.wishlistDraftId ?? 0)")
+
+                    completion(true)
+                    self.navigate = true
+                    
+                    print("add to userDefualt successfully!!!")
+                } else {
+                    completion(false)
+                    self.navigate = false
+                    print("No customer found in the response.")
+                }
+            }
+        }
+    }
+    
+    func createDraftOrder() {
+        let dummyLineItem :[String : Any] =
+        [
+            "title": "dummy",
+            "quantity": 1,
+            "price": "0.0",
+            "properties":[]
+        ]
+        
+        let apiUrl = APIHandler.urlForGetting(.draftOrders)
+        networkHandler?.postWithResponse(url: apiUrl, type: DraftOrderContainer.self, parameters: ["draft_order": ["line_items":[dummyLineItem]]], completion: { container in
+            
+            self.cartDraftId = container?.draftOrder.id
+        })
+        Thread.sleep(forTimeInterval: 1)
+        
+        networkHandler?.postWithResponse(url: apiUrl, type: DraftOrderContainer.self, parameters: ["draft_order": ["line_items":[dummyLineItem]]], completion: { container in
+            
+            self.wishlistDraftId = container?.draftOrder.id
+        })
+        Thread.sleep(forTimeInterval: 1)
+        self.note = "\(self.cartDraftId ?? 0),\(self.wishlistDraftId ?? 0)"
+
+    }
+    func addNoteToCustomer( id : Int) {
+        
+        var customerNote: [String: [String: String]] = [
+            "customer": [
+                "note":"\(self.cartDraftId ?? 0),\(self.wishlistDraftId ?? 0)"
+            ]
+        ]
+        let apiUrl = APIHandler.urlForGetting(.Customer(id: String(id)))
+        print("url:\(apiUrl)")
+        print("put in api : cartId:\(self.cartDraftId ?? 0) wishlist Id :\(self.wishlistDraftId ?? 0)")
+        networkHandler?.putInApi(url: apiUrl, parameters:customerNote)
+        
+    }
+        
+        
+    
+}
+//                    //dummy order
+//                    let dummyLineItem: [String: Any] = ["title": "dummy", "quantity": 1, "price": "0.0", "properties":[]]
+//                    let order = Order(id: 0, lineItems: [LineItem(id: 0, variantID: nil, productID: nil, price: "12.25", name: "45", title: "title", quantity: 1, properties: [])], createdAt: "2024-03-05T14:14:42-05:00", currency: UserDefaults.standard.string(forKey: "currencyTitle") ?? "", currentSubtotalPrice: "", name: "", subtotalPrice: "", totalPrice: "", customer: CustomerModel(first_name: "", last_name: "", email: "", phone: "+13125551212", tags: "", id: self.userDefualt.getCustomerId(), verified_email: false, note: ""), currentTotalDiscounts: "", totalDiscounts: "",appliedDiscount: nil)
+//                    let parameters = HelperFunctions().convertToDictionary(object: order, String: "order") ?? [:]
+//                    self.networkHandler?.PostToApi(url: APIHandler.urlForGetting(.orders), parameters: parameters)
+       

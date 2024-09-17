854aaeeead9bbce2c775ca254c59b7ceb4f45815
Elsayed Mansour <sayedmansoo123@gmail.com>
2024-09-17 16:34:29 +0300

    upload sign in logic and its view model

--- ShopyApp/Modules/MainScene/Authorization/View/SignInViewController.swift
+++ ShopyApp/Modules/MainScene/Authorization/View/SignInViewController.swift
@@ -11,15 +11,124 @@ class SignInViewController: UIViewController {
     @IBOutlet weak var emailText: UITextField!
     
     @IBOutlet weak var passwordText: UITextField!
+    
+    var _firstName, _lastName, _email, _password: String!
+    
+    let userDefualt = Utilities()
+    
+    var signInViewModel : SignInViewModel?
+    
+    var note: String! = "0"
+    var navigate: Bool! = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        configureLoadingData()
+    }
+    
+    func navigateToHome(){
+    
+        if userDefualt.isLoggedIn() {
+            performSegue(withIdentifier: "logIn", sender: self)
+        }else {
+            print("failed")
+            let alert = UIAlertController(title: "Not Authorized", message: "user not exist, please check your email" , preferredStyle: .alert)
+            let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+            alert.addAction(gotIt)
+            self.present(alert, animated: true)
+            
+        }
+    }
+    
+    func configureLoadingData(){
+        print("configureLoadingData")
+        signInViewModel = SignInViewModel()
+        print("1")
+        signInViewModel?.loadData()
+        print("2")
+        signInViewModel?.bindResultToViewController = { [weak self] in
+            DispatchQueue.main.async {
+                
+                print("customers in sign in screen :\n")
+                print(self?.signInViewModel?.getAllCustomers()?[0].email ?? "")
+                guard let customers = self?.signInViewModel?.getAllCustomers() else {
+                    print ("failed to get customers")
+                    return
+                }
+            }
+        }
         
     }
     
-
+    @IBAction func signInButton(_ sender: Any) {
+        signInViewModel?.checkNetworkReachability{ isReachable in
+            if isReachable {
+                self._email = self.emailText.text
+                self._password = self.passwordText.text
+                if self.userDefualt.isValidEmail(self._email){
+                    if self._password.count >= 6 {
+                        for item in (self.signInViewModel?.getAllCustomers())! {
+                            
+                            let comingMail = item.email ?? ""
+                            print(comingMail)
+                            if comingMail == self._email{
+                                print("matched EMail:\(comingMail)")
+                                
+                                self.userDefualt.login()
+                                self.userDefualt.addId(id: item.id ?? 0)
+                                self.userDefualt.addCustomerName(customerName: "\(item.first_name!.split(separator: " ", maxSplits: 1, omittingEmptySubsequences: true)[0]) \(item.last_name!)")
+                                self.userDefualt.setUserPassword(password: item.tags ?? "")
+                                self.userDefualt.addCustomerEmail(customerEmail: item.email ?? "")
+                                self.userDefualt.setUserNote(note: item.note ?? "")
+                                let array = item.note?.components(separatedBy: ",")
+                                self.userDefualt.setCartID(cartId: array?[0] ?? "0")
+                                print("wishlist:\(self.userDefualt.getWishlistID())")
+                                print("cart\(self.userDefualt.getCartID())")
+                                self.userDefualt.setWishlistID(wishlistId: array?[1] ?? "0")
+                                print("Utilities.utilities.getUserNote()\(Utilities.utility.getUserNote())")
+                                
+                                self.note = item.note
+                                
+                                break
+                            }
+                        }
+                        self.navigateToHome()
+                    }else{
+                        let alert = UIAlertController(title: "Not Authorized", message: "Password should be 6 characters at least" , preferredStyle: .alert)
+                        let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                        alert.addAction(gotIt)
+                        self.present(alert, animated: true)
+                        self.navigate = false
+                    }
+                }else{
+                    let alert = UIAlertController(title: "Not Authorized", message: "Please enter a valid email" , preferredStyle: .alert)
+                    let gotIt = UIAlertAction(title: "Try Again", style: .cancel)
+                    alert.addAction(gotIt)
+                    self.present(alert, animated: true)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    self.showAlert()
+                }
+            }
+        }
+    }
+    
    
     @IBAction func backButton(_ sender: Any) {
         dismiss(animated: true)
+    }
+    
+    func showAlert(){
+        let alertController = UIAlertController(title: "No Internet Connection", message: "Check your network and try again", preferredStyle: .alert)
+        
+        let doneAction = UIAlertAction(title: "Retry", style: .cancel) { _ in
+            self.viewWillAppear(true)
+        }
+        
+        alertController.addAction(doneAction)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 }
--- ShopyApp/Modules/MainScene/Authorization/ViewModel/SignInViewModel.swift
+++ ShopyApp/Modules/MainScene/Authorization/ViewModel/SignInViewModel.swift
@@ -6,3 +6,52 @@
 //
 
 import Foundation
+
+class SignInViewModel{
+    var networkHandler:NetworkManager?
+    let model = ReachabilityManager()
+    let userDefualt = Utilities()
+    var bindResultToViewController : (()->()) = {}
+    
+    init() {
+        self.networkHandler = NetworkManager()
+    }
+    var listOfCustomer : [CustomerModel]?{
+        didSet{
+            bindResultToViewController()
+        }
+    }
+    func getAllCustomers()-> [CustomerModel]?{
+        return listOfCustomer
+    }
+    func checkNetworkReachability(completion: @escaping (Bool) -> Void) {
+        model.checkNetworkReachability { isReachable in
+            completion(isReachable)
+        }
+    }
+    //https://a73c5fc1c095fd186d957dd2093e9006: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@q2-24-team2.myshopify.com/admin/api/2024-01/draft_orders.json
+    //draft_orders.json
+    func loadData() {
+        let apiUrl = "https://a73c5fc1c095fd186d957dd2093e9006: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@q2-24-team2.myshopify.com/admin/api/2024-01/customers.json?since_id=1"
+        print(apiUrl)
+        networkHandler?.fetchData(url: apiUrl, type: AllCustomers.self,complitionHandler:  { data in
+            //print("the data from fetching all customers\(data?.customers?.count ?? 0)")
+            if let data = data{
+                self.listOfCustomer = data.customers
+                print("no of customers in load data\(self.listOfCustomer?.count ?? 0)")
+            }else {
+                print("error in getting all customers")
+                
+            }
+        }, headers: [
+            "Cookie":"",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        ])
+    }
+    
+    func checkDraftOrderInUser(){}
+    
+   
+    
+}
